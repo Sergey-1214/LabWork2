86aa41eb232ea0429eefe675819d0c882a632423
Sergey Pavlov <serezha.pavlov.06@inbox.ru>
2024-12-24 07:14:38 +0300

    Добавлен новый файл

---
+++ num1.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print('Hello', name)
+
+
+
+
+
+def square(number):
+    return number**2
+
+
+
+
+
+def max_of_two(x,y):
+    if x>y: return x
+    elif x<y: return y
+    else: return 'Числа равны'
